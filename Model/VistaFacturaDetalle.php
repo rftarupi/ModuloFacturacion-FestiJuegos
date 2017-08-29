4fd1eb7a40615a205b8958f1e1f2886b7741a50e
kdrodriguez <kevin.rzz18@gmail.com>
2017-08-29 15:59:32 -0500

    Implementacion de los archivos TRIGGER Y MODIFICACIONES BDD, los cuales contienen los triggers de actualizacion de iva, subtotal, total y la adición de los campos de subtotal, iva y total, ademas de la creacion d la clase Modelo de detalles factura y la clse vista de detalles

---
+++ Model/VistaFacturaDetalle.php
@@ -0,0 +1,65 @@
+<?php
+// Clase que simula una vista de un detalle de factura
+
+class FacturaDetalle {
+
+    // Atributos de Usuarios
+    private $COD_DET_FACT;
+    private $NOMBRE_SERV;
+    private $TIEMPO_DET_FACT;
+    private $COSTO_HORA_DET_FACT;
+    private $COSTO_TOT_DET_FACT;
+    
+    // Constructor   
+    function __construct($COD_DET_FACT, $NOMBRE_SERV, $TIEMPO_DET_FACT, $COSTO_HORA_DET_FACT, $COSTO_TOT_DET_FACT) {
+        $this->COD_DET_FACT = $COD_DET_FACT;
+        $this->NOMBRE_SERV = $NOMBRE_SERV;
+        $this->TIEMPO_DET_FACT = $TIEMPO_DET_FACT;
+        $this->COSTO_HORA_DET_FACT = $COSTO_HORA_DET_FACT;
+        $this->COSTO_TOT_DET_FACT = $COSTO_TOT_DET_FACT;
+    }
+
+        //Métodos Getter y Setter
+        function getCOD_DET_FACT() {
+            return $this->COD_DET_FACT;
+        }
+
+        function getNOMBRE_SERV() {
+            return $this->NOMBRE_SERV;
+        }
+
+        function getTIEMPO_DET_FACT() {
+            return $this->TIEMPO_DET_FACT;
+        }
+
+        function getCOSTO_HORA_DET_FACT() {
+            return $this->COSTO_HORA_DET_FACT;
+        }
+
+        function getCOSTO_TOT_DET_FACT() {
+            return $this->COSTO_TOT_DET_FACT;
+        }
+
+        function setCOD_DET_FACT($COD_DET_FACT) {
+            $this->COD_DET_FACT = $COD_DET_FACT;
+        }
+
+        function setNOMBRE_SERV($NOMBRE_SERV) {
+            $this->NOMBRE_SERV = $NOMBRE_SERV;
+        }
+
+        function setTIEMPO_DET_FACT($TIEMPO_DET_FACT) {
+            $this->TIEMPO_DET_FACT = $TIEMPO_DET_FACT;
+        }
+
+        function setCOSTO_HORA_DET_FACT($COSTO_HORA_DET_FACT) {
+            $this->COSTO_HORA_DET_FACT = $COSTO_HORA_DET_FACT;
+        }
+
+        function setCOSTO_TOT_DET_FACT($COSTO_TOT_DET_FACT) {
+            $this->COSTO_TOT_DET_FACT = $COSTO_TOT_DET_FACT;
+        }
+
+
+      
+}
